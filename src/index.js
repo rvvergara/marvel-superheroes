01263449497489464554caaccb4f2c30424406d8
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-10-12 10:26:00 +0800

    Make characters fetch work

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 require('dotenv').config();
+
 const express = require('express');
+
+const axios = require('axios');
+
+const CryptoJS = require('crypto-js');
 
 const app = express();
 
 const port = process.env.PORT;
 
+const marvelURL = 'http://gateway.marvel.com';
+
 const publicKey = process.env.PUBLIC_KEY;
 
+const generateTs = () => new Date().getTime();
+
 const privateKey = process.env.PRIVATE_KEY;
+
+const generateHash = (ts) => CryptoJS.MD5(`${generateTs()}${privateKey}${publicKey}`);
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  res.send(`Get ready for the superheroes! Your public key is ${publicKey}. Your private key is ${privateKey}`);
+  res.send(`Get ready for the superheroes! Your hash is ${generateHash()}`);
+});
+
+app.get('/characters', async (req, res) => {
+  const ts = generateTs();
+  const hash = generateHash(ts);
+  const path = `/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=100`;
+
+  const response = await axios.get(`${marvelURL}${path}`);
+
+  const ids = response.data.data.results.map(char => char.id);
+
+  res.send(`Here is your array -> ${ids}`);
 });
 
 app.listen(port, () => {
