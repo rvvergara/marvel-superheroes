68b42c0e66099b4b688c43d1188e114c4f92cf9c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-10-13 10:54:53 +0800

    Create cache middleware

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -4,4 +4,35 @@ const redisPort = process.env.REDIS_PORT;
 
 const redisClient = redis.createClient(redisPort);
 
-const set = 
+const setCharactersInCache = (req, res, next) => {
+  const { data } = res.locals;
+  const charactersPage = data.page;
+
+  const key = `charactersPage${charactersPage}`;
+
+  redisClient.setex(key, 3600, JSON.stringify(data));
+
+  return next();
+}
+
+const getCharactersFromCache = async (req, res, next) => {
+  const charactersPage = req.query.page || 1;
+
+  const key = `charactersPage${charactersPage}`;
+
+  redisClient.get(key, (err, data) => {
+    if(err){
+      res.status(500).send(err);
+    } else if(data !== null) {
+      return res.status(200).send(data);
+    } else {
+      return next();
+    }
+  })
+
+}
+
+module.exports = {
+  setCharactersInCache,
+  getCharactersFromCache
+}
